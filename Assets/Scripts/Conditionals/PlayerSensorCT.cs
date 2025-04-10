f8ca8675d08640364e571bf16f6461b2803c9c7d
T4lynn <148375548+T4lynn@users.noreply.github.com>
2025-04-10 11:29:50 -0400

    fixed overlap sphere detection
    - need to delete extra 'overlap' cs scripts
    - used binary selector to sense the player rather than a selector + conditional

--- Assets/Scripts/Conditionals/PlayerSensorCT.cs
+++ Assets/Scripts/Conditionals/PlayerSensorCT.cs
@@ -1,14 +1,19 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-
+using UnityEngine;
 
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class PlayerSensorCT : ConditionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit(){
+        public float radius;
+        Vector3 sphereRadius;
+        public BBParameter<bool> sensedPlayer;
+		public Transform guardTransform;
+		public Collider[] hitColliders;
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit(){
 			return null;
 		}
 
@@ -17,7 +22,7 @@ namespace NodeCanvas.Tasks.Conditions {
 			
 		}
 
-		//Called whenever the condition gets disabled.
+		//Called whenever the condition gets disabled. 
 		protected override void OnDisable() {
 			
 		}
@@ -25,7 +30,24 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+            sphereRadius = new Vector3(guardTransform.position.x + radius, guardTransform.position.y + radius, guardTransform.position.z + radius);
+			hitColliders = Physics.OverlapSphere(guardTransform.position, radius);
+            Debug.DrawLine(guardTransform.position, sphereRadius, Color.red);
+
+            if ( hitColliders.Length > 0)
+            {
+                Debug.Log("Player in sphere");
+
+                sensedPlayer.value = true;
+				return true;
+            }
+            else 
+			{ 
+				Debug.Log("player out of sphere");
+				sensedPlayer.value = false;
+                return false;
+            }
+			
 		}
 	}
 }
